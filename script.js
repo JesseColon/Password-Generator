57e01881197189ec68f0676b84bb1fe1ee770179
AzureSol <jay.alexandervii@gmail.com>
2023-04-06 22:11:28 -0400

    got basic password to pront

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-const randomFunc = {
-   lower: getRandomLower,
-   upper: getRandomUpper,
-   number: getRandomNumber,
-   symbol: getRandomSymbol
-  };
   
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -29,54 +22,70 @@ function getRandomSymbol() {
 
 generateBtn.addEventListener("click", writePassword)
 
-function generatePassword (length, lower, upper, number, symbol){
+function generatePassword (){
  
  
 
-  var lengthEl=prompt("how long would you like the password to be (enter a number between 8 and 128)","" )
+  var lengthEl=prompt("how long would you like the password to be (enter a number between 8 and 128)","" );
  
-  if (lengthEl < 8) {alert("pasword length must be at least 8 characters")
+  if (lengthEl < 8) {alert("pasword length must be at least 8 characters");
 return null
 }
-  if (lengthEl > 128) {alert("password length must be at most 128 characters")
+  if (lengthEl > 128) {alert("password length must be at most 128 characters");
 return null
 }
 
-if (isNaN(lengthEl)){alert("plz only enter a number between 8 and 128")
+if (isNaN(lengthEl)){alert("plz only enter a number between 8 and 128");
 return null
 }
  
- var lowerEl=confirm("would you like to include lowercase letters")
+ var lowerEl=confirm("would you like to include lowercase letters");
  
-  var upperEl=confirm("would you like to include uppercase letters")	
+  var upperEl=confirm("would you like to include uppercase letters");	
 
-  var symbolEl=confirm("would you like to include symbols")
+  var symbolEl=confirm("would you like to include symbols");
 
-  var numberEl=confirm("would you like to include numbers")
+  var numberEl=confirm("would you like to include numbers");
 
   if (lowerEl === false && upperEl === false && symbolEl === false && numberEl === false){
-    alert("must at least choose 1 character set")
-    return null
+    alert("must at least choose 1 character set");
+    return null;
   }
-
-  {	
+  var passwordResult = "";
+  if(lowerEl){
+    for(var i = 0 ;i < lengthEl; i++){
+        passwordResult = passwordResult + getRandomLower();
     
-}
-
-var typescount = lower + upper + number + symbol;
-
-  var typesArray = [{upper}, {lower}, {number}, {symbol}].filter
-  (
-    item => Object.values(item)[0]
-  );
-
-  for(let i = 0; i < length; i += typescount) {
-    typesArray.forEach(type => {
-      const funcName = object.key(type)[0];
-
-     passwordText += randomFunc[funcName]();
-    });
   }
+  if (upperEl){
+    passwordResult = passwordResult + getRandomUpper();
+  }
+
+  if (symbolEl){
+    passwordResult = passwordResult + getRandomSymbol();
+  }
+
+  if (numberEl){
+    passwordResult = passwordResult + getRandomNumber();
+  }
+}
+  return passwordResult;
+
+
+// var typescount = lower + upper + number + symbol;
+
+//   var typesArray = [{upper}, {lower}, {number}, {symbol}].filter
+//   (
+//     item => Object.values(item)[0]
+//   );
+
+  // for(let i = 0; i < length; i += typescount) {
+  //   typesArray.forEach(type => {
+  //     const funcName = object.key(type)[0];
+
+  //    passwordText += randomFunc[funcName]();
+  //   });
+  // }
 
 }
 
@@ -90,3 +99,5 @@ function writePassword() {
 
    
 }
+
+wftbciunzvnrcwzljlrhsqwyxeljiszwzjT,6
